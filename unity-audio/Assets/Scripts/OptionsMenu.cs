c3e82970418e3197bdea353aa11be3a94de3d77c
SergioLebron22 <lebronsergio22@gmail.com>
2025-04-05 16:22:07 -0400

    feat: add BGM volume control to OptionsMenu and implement volume persistence

--- unity-audio/Assets/Scripts/OptionsMenu.cs
+++ unity-audio/Assets/Scripts/OptionsMenu.cs
@@ -1,30 +1,60 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
+    public Slider BGMSlider;
+    public AudioMixer audioMixer;
+    private const string BGMVolumeParam = "BGMVolume";  
+    private const string VolumeKey = "BGMVolumeSaved";
     private static int previousSceneIndex = 0;
     public Toggle invertYToggle;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private float originalVolume;
+
     void Start()
     {
         previousSceneIndex = PlayerPrefs.GetInt("PreviousScene", 0);
-        Debug.Log(previousSceneIndex);
 
         bool isInverted = PlayerPrefs.GetInt("InvertY", 0) == 1;
         invertYToggle.isOn = isInverted;
+
+        originalVolume = PlayerPrefs.GetFloat(VolumeKey, 1f);
+        BGMSlider.value = originalVolume;
+
+        SetMixerVolume(originalVolume);
     }
-    public void Back() {
-        if (previousSceneIndex != SceneManager.GetActiveScene().buildIndex) {
+
+    public void Back()
+    {
+        BGMSlider.value = originalVolume;
+        SetMixerVolume(originalVolume);
+
+        if (previousSceneIndex != SceneManager.GetActiveScene().buildIndex)
+        {
             SceneManager.LoadScene(previousSceneIndex);
         }
     }
 
-    public void Apply() {
+    public void Apply()
+    {
         PlayerPrefs.SetInt("InvertY", invertYToggle.isOn ? 1 : 0);
+
+        float newVolume = BGMSlider.value;
+        PlayerPrefs.SetFloat(VolumeKey, newVolume);
         PlayerPrefs.Save();
+
+        SetMixerVolume(newVolume);
+        originalVolume = newVolume; 
+
         Back();
+    }
+
+    private void SetMixerVolume(float value)
+    {
+        float volumeInDb = Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20f;
+        audioMixer.SetFloat(BGMVolumeParam, volumeInDb);
     }
 }
